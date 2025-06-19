7badcce96d114e7b531c7e02b474270efe6361ef
learnerleko12 <160587555+learnerleko12@users.noreply.github.com>
2025-06-19 14:05:13 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,25 @@
 import streamlit as st
-
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
 
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 
+# Streamlit 파일 업로드
 json_keyfile = st.file_uploader("🔑 서비스 계정 키(JSON)", type="json")
 
 if json_keyfile is not None:
     with open("temp_key.json", "wb") as f:
         f.write(json_keyfile.read())
-    
+
     creds = ServiceAccountCredentials.from_json_keyfile_name("temp_key.json", scope)
     client = gspread.authorize(creds)
 
+    # 구글 시트 URL 입력받기
+    sheet_url = st.text_input("https://docs.google.com/spreadsheets/d/1QWoRvGJW-QbBwFEdUXlaMyT7QBl7V4PhGpkTdFU6uZU/edit?usp=sharing")
 
-
-sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1QWoRvGJW-QbBwFEdUXlaMyT7QBl7V4PhGpkTdFU6uZU/edit?usp=sharing").sheet1
-data = sheet.get_all_records()
-print(data)
+    if sheet_url:
+        sheet = client.open_by_url(sheet_url).sheet1
+        data = sheet.get_all_records()
+        df = pd.DataFrame(data)
+        st.write("📊 시트 데이터 미리보기", df)
